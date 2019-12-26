35cce00b28998a4818500c08717203f446eebbd0
Travis <tssund93@gmail.com>
2019-12-26 17:25:52 -0600

    init

---
+++ tenhou-dl-gui.asd
@@ -0,0 +1,8 @@
+(asdf:defsystem #:tenhou-dl-gui
+  :description "Describe tenhou-dl here"
+  :author "Your Name <your.name@example.com>"
+  :license  "Specify license here"
+  :version "0.0.1"
+  :serial t
+  :depends-on (:tenhou-dl)
+  :components ((:file "tenhou-dl-gui")))
---
+++ tenhou-dl-gui.lisp
@@ -0,0 +1,36 @@
+(defpackage #:tenhou-dl-gui
+  (:use #:cl #:capi #:tenhou-dl))
+
+(in-package #:tenhou-dl-gui)
+
+(defun create-window ()
+  (let ((tenhou-id-input (make-instance 'text-input-pane :title "Tenhou ID"))
+        (save-path-input (make-instance 'text-input-pane :title "Save Path"))
+        (output-textbox (make-instance 'collector-pane :enabled nil)))
+    (contain
+     (make-instance
+      'column-layout
+      :description
+      (list tenhou-id-input
+            save-path-input
+            (make-instance
+             'push-button
+             :data "Download"
+             :default-p t
+             :callback-type :item
+             :callback
+             (lambda (item)
+               (cond ((or (= 0 (length (text-input-pane-text tenhou-id-input)))
+                          (= 0 (length (text-input-pane-text save-path-input))))
+                      (display-message "Tenhou ID and save path are required."))
+                     (t (setf (button-enabled item) nil)
+                        (bordeaux-threads:make-thread
+                         (lambda ()
+                           (let ((*standard-output* (collector-pane-stream output-textbox)))
+                             (format t "~%Downloaded ~a replay~:p~%"
+                                     (length (download-replays
+                                              (text-input-pane-text tenhou-id-input)
+                                              (text-input-pane-text save-path-input))))
+                             (setf (button-enabled item) t))))))))
+            :separator
+            output-textbox)))))
