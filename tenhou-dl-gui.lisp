a8cca61057b3eba95fc3b9ea225a019198e599f4
Travis <tssund93@gmail.com>
2019-12-31 12:15:02 -0600

    use define-interface

--- tenhou-dl-gui.lisp
+++ tenhou-dl-gui.lisp
@@ -3,42 +3,40 @@
 
 (in-package #:tenhou-dl-gui)
 
-(defun create-window ()
-  (let ((tenhou-id-input (make-instance 'text-input-pane :title "Tenhou ID"))
-        (save-path-input (make-instance 'text-input-pane
-                                        :title "Save Path"
-                                        :file-completion t
-                                        :directories-only t
-                                        :editing-callback
-                                        (lambda (pane type)
-                                          (when (eql type :start)
-                                            (text-input-pane-complete-text pane)))))
-        (output-textbox (make-instance 'collector-pane :enabled nil)))
-    (contain
-     (make-instance
-      'column-layout
-      :description
-      (list tenhou-id-input
-            save-path-input
-            (make-instance
-             'push-button
-             :data "Download"
-             :default-p t
-             :callback-type :item
-             :callback
-             (lambda (item)
-               (cond ((or (= 0 (length (text-input-pane-text tenhou-id-input)))
-                          (= 0 (length (text-input-pane-text save-path-input))))
-                      (display-message "Tenhou ID and save path are required."))
-                     (t (setf (button-enabled item) nil)
-                        (bt:make-thread
-                         (lambda ()
-                           (let ((*standard-output* (collector-pane-stream output-textbox)))
-                             (unwind-protect
-                                 (format t "~%Downloaded ~a replay~:p~%"
-                                         (length (download-replays
-                                                  (text-input-pane-text tenhou-id-input)
-                                                  (text-input-pane-text save-path-input))))
-                               (setf (button-enabled item) t)))))))))
-            :separator
-            output-textbox)))))
+(define-interface window ()
+  ()
+  (:panes (tenhou-id-input text-input-pane :title "Tenhou ID")
+          (save-path-input text-input-pane :title "Save Path"
+                           :file-completion t
+                           :directories-only t
+                           :editing-callback
+                           (lambda (pane type)
+                             (when (eql type :start)
+                               (text-input-pane-complete-text pane))))
+          (output-textbox collector-pane :enabled nil)
+          (download-button push-button
+                           :data "Download"
+                           :default-p t
+                           :callback-type :item
+                           :callback
+                           (lambda (item)
+                             (cond ((or (= 0 (length (text-input-pane-text tenhou-id-input)))
+                                        (= 0 (length (text-input-pane-text save-path-input))))
+                                    (display-message "Tenhou ID and save path are required."))
+                                   (t (setf (button-enabled item) nil)
+                                      (bt:make-thread
+                                       (lambda ()
+                                         (let ((*standard-output* (collector-pane-stream output-textbox)))
+                                           (unwind-protect
+                                               (format t "~%Downloaded ~a replay~:p~%"
+                                                       (length (download-replays
+                                                                (text-input-pane-text tenhou-id-input)
+                                                                (text-input-pane-text save-path-input))))
+                                             (setf (button-enabled item) t))))))))))
+  (:layouts (main-layout column-layout
+                         '(tenhou-id-input
+                           save-path-input
+                           download-button
+                           :separator
+                           output-textbox)))
+  (:default-initargs :title "Tenhou DL"))
