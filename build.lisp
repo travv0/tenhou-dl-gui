a2fe273e1d015b51220c91725df451463513dd67
Travis <tssund93@gmail.com>
2020-01-02 20:35:34 -0600

    update deliver function call to make a mac app

--- build.lisp
+++ build.lisp
@@ -1,3 +1,8 @@
-(load "~/.lispworks")
+(lw:set-default-character-element-type 'character)
+(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
+                                       (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (load quicklisp-init)))
+(push #P"~/code/" ql:*local-project-directories*)
 (ql:register-local-projects)
 (ql:quickload :tenhou-dl-gui)
--- deliver.lisp
+++ deliver.lisp
@@ -5,9 +5,19 @@
 (load-all-patches)
 
 ;;; Load the application:
-(load "~/common-lisp/tenhou-dl-gui/build.lisp")
+(load "~/code/common-lisp/tenhou-dl-gui/build.lisp")
 
-(deliver 'tenhou-dl-gui:start "~/common-lisp/tenhou-dl-gui/tenhou-dl" 4
+(deliver 'tenhou-dl-gui:start
+         #+:cocoa
+         (create-macos-application-bundle
+          "~/Desktop/Tenhou DL.app"
+          ;; Do not copy file associations...
+          :document-types nil
+          ;; ...or CFBundleIdentifier from the LispWorks bundle
+          ;; :identifier "com.example.Hello"
+          )
+         #-:cocoa "~/code/common-lisp/tenhou-dl-gui/tenhou-dl"
+         4
          :interface :capi
          :startup-bitmap-file nil
          :keep-top-level nil)
